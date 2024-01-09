1f452cd9009768791e8eeb891e066b924d22549f
吴培铭 <1780439574@qq.com>
2024-01-09 17:22:16 +0800

    feature: 修改操作日志页面

--- src/views/logManage/index.vue
+++ src/views/logManage/index.vue
@@ -6,71 +6,76 @@
             content="展示所有增删改等历史记录，您可以根据操作者/操作类型/时间等条件进行查询" />
         <div class="search-box">
             <span>操作者</span>
-            <bk-input
-                :clearable="true"
+            <el-input
+                clearable
+                size="small"
                 v-model="params.operator"
                 style="width: 150px;"
                 placeholder="请输入操作者"
-                @change="searchDataByUser"
-            >
-            </bk-input>
+                @change="searchDataByUser">
+            </el-input>
             <span>操作类型</span>
-            <bk-select
+            <el-select
+                size="small"
                 v-model="params.operate_type"
                 style="width: 150px;background-color: #ffffff;"
-                placeholder="请选择操作类型"
-                ext-popover-cls="select-popover-custom"
-                @change="searchDataByType"
-            >
-                <bk-option v-for="option in typeList"
-                    :key="option.id"
-                    :id="option.id"
-                    :name="option.name"
-                >
-                </bk-option>
-            </bk-select>
+                clearable placeholder="请选择操作类型"
+                @change="searchDataByType">
+                <el-option
+                    v-for="item in typeList"
+                    :key="item.id"
+                    :label="item.name"
+                    :value="item.id">
+                </el-option>
+            </el-select>
             <span>时间范围</span>
-            <bk-date-picker
+            <el-date-picker
                 class="mr16"
+                size="small"
                 v-model="params.dateTime"
-                :placeholder="'请选择时间范围'"
-                :type="'datetimerange'"
-                @change="getDate"
-            >
-            </bk-date-picker>
-            <bk-button
-                :theme="'default'"
-                :title="'重置'"
-                @click="resetSearch"
-                :disabled="isLoading">
-                重置
-            </bk-button>
+                type="datetimerange"
+                range-separator="-"
+                start-placeholder="开始日期"
+                end-placeholder="结束日期"
+                @change="getDate">
+            </el-date-picker>
+            <el-button size="small" :disabled="isLoading" @click="resetSearch">重置</el-button>
         </div>
         <div class="test-dom" v-bkloading="{ isLoading: isLoading, zIndex: 10 }">
-            <bk-table
+            <el-table
+                border
                 :data="logList"
                 :size="size"
-                :pagination="pagination"
                 :max-height="maxHeight"
-                @page-change="handlePageChange"
-                @page-limit-change="handleLimitChange"
                 ref="table"
                 style="margin-top: 10px;background-color: #ffffff;"
             >
-                <bk-table-column label="操作者" prop="operator" width="200"></bk-table-column>
-                <bk-table-column label="操作对象" prop="operate_obj" width="200"></bk-table-column>
-                <bk-table-column label="操作类型" prop="operate_type" width="200">
+                <el-table-column label="操作者" prop="operator" width="200"></el-table-column>
+                <el-table-column label="操作对象" prop="operate_obj" width="200"></el-table-column>
+                <el-table-column label="操作类型" prop="operate_type" width="200">
                     <template slot-scope="props">
                         <span>{{ typeMap[props.row.operate_type]}}</span>
                     </template>
-                </bk-table-column>
-                <bk-table-column label="操作时间" prop="created_at" width="200"></bk-table-column>
-                <bk-table-column
+                </el-table-column>
+                <el-table-column label="操作时间" prop="created_at" width="200"></el-table-column>
+                <el-table-column
                     :show-overflow-tooltip="true"
                     label="概要"
                     prop="operate_summary">
-                </bk-table-column>
-            </bk-table>
+                </el-table-column>
+            </el-table>
+            <div class="pager">
+                <el-pagination
+                    background
+                    @size-change="handleLimitChange"
+                    @current-change="handlePageChange"
+                    :current-page="1"
+                    :page-sizes="[10, 20, 50, 100]"
+                    :page-size="100"
+                    layout="total, sizes, prev, pager, next"
+                    :total="pagination.count">
+                </el-pagination>
+            </div>
         </div>
     </div>
 </template>
@@ -262,6 +267,14 @@
         & > span:nth-of-type(1) {
             margin-left: 0;
         }
+    }
+    .pager {
+        padding: 15px;
+        border: 1px solid #dfe0e5;
+        background-color: rgb(255, 255, 255);
+        // .btn-prev, .el-pager, .btn-next {
+        //     float: right;
+        // }
     }
 }
 </style>
