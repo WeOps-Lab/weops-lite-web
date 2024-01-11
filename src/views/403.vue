6017914c8fa3f746d34d9d526d4530ef4f2520ff
吴培铭 <1780439574@qq.com>
2024-01-11 10:52:47 +0800

    feature: 切换异常提示页面UI

--- src/views/403.vue
+++ src/views/403.vue
@@ -1,24 +1,18 @@
 <template>
     <div>
-        <bk-exception class="exception-wrap-item" type="403">
+        <el-empty description=" ">
             <span>抱歉，您暂无该功能的权限</span>
             <div class="text-subtitle">你没有该功能的权限，请联系您的专属客户经理进行申请</div>
-        </bk-exception>
+        </el-empty>
     </div>
 </template>
 
 <style lang="scss">
-.exception-wrap-item {
-    margin: 10px;
-    height: 420px;
-    padding-top: 22px;
-    .text-wrap {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        color: #3A84FF;
-        font-size: 14px;
-        margin-top: 12px;
+/* stylelint-disable selector-class-pattern */
+.el-empty__bottom {
+    span {
+        font-size: 24px;
+        color: #63656e;
     }
     .text-subtitle {
         color: #979BA5;
--- src/views/authPermission.vue
+++ src/views/authPermission.vue
@@ -1,8 +1,8 @@
 <template>
-    <bk-exception class="exception-wrap-item" type="403">
+    <el-empty description=" ">
         <span>无菜单权限</span>
         <div class="text-subtitle" style="margin-top: 20px;">您没有相应菜单的访问权限，请前往申请相关菜单权限</div>
-    </bk-exception>
+    </el-empty>
 </template>
 
 <script lang="ts">
@@ -12,3 +12,13 @@
     })
     export default class NoPurview extends Vue {}
 </script>
+
+<style lang="scss">
+/* stylelint-disable selector-class-pattern */
+.el-empty__bottom {
+    color: #63656e;
+    font-size: 24px;
+    text-align: center;
+    margin-top: 14px;
+}
+</style>
