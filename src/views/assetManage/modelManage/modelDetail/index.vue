72907e8632772d87edf2a10b9757c26bfe7fa6f5
CharlesWhj <1253297146@qq.com>
2024-04-25 11:31:35 +0800

    optimization: 模型管理添加操作权限

--- src/views/assetManage/modelManage/modelDetail/index.vue
+++ src/views/assetManage/modelManage/modelDetail/index.vue
@@ -15,14 +15,42 @@
                     </div> -->
             </div>
             <div class="header-operate">
-                <el-button type="text" size="mini" icon="el-icon-edit" @click="editModel">编辑</el-button>
-                <el-button type="text" size="mini" icon="el-icon-delete" @click="handleDelete">删除</el-button>
+                <el-button
+                    v-permission="{
+                        id: $route.name,
+                        type: 'ModelManage_edit'
+                    }"
+                    type="text"
+                    size="mini"
+                    icon="el-icon-edit"
+                    @click="editModel">
+                    编辑
+                </el-button>
+                <el-button
+                    v-permission="{
+                        id: $route.name,
+                        type: 'ModelManage_delete'
+                    }"
+                    type="text"
+                    size="mini"
+                    icon="el-icon-delete"
+                    @click="handleDelete">
+                    删除
+                </el-button>
             </div>
         </div>
         <el-card class="mt10">
             <menu-tab type="line" :panels="panels" :active-panel="active" @click="toTabMenu"></menu-tab>
             <div v-if="active === 'property'">
-                <el-button class="mt20 mb15" size="small" type="primary" @click="editAttr()">
+                <el-button
+                    v-permission="{
+                        id: $route.name,
+                        type: 'ModelManage_create'
+                    }"
+                    class="mt20 mb15"
+                    size="small"
+                    type="primary"
+                    @click="editAttr()">
                     添加属性
                 </el-button>
                 <com-table
@@ -32,8 +60,26 @@
                     :row-key="'attr_id'"
                     :height="tableMaxHeight">
                     <template slot="operation" slot-scope="{ row }">
-                        <el-button type="text" size="mini" @click="editAttr(row)">编辑</el-button>
-                        <el-button type="text" size="mini" @click="deleteAtrr(row)">删除</el-button>
+                        <el-button
+                            v-permission="{
+                                id: $route.name,
+                                type: 'ModelManage_edit'
+                            }"
+                            type="text"
+                            size="mini"
+                            @click="editAttr(row)">
+                            编辑
+                        </el-button>
+                        <el-button
+                            v-permission="{
+                                id: $route.name,
+                                type: 'ModelManage_delete'
+                            }"
+                            type="text"
+                            size="mini"
+                            @click="deleteAtrr(row)">
+                            删除
+                        </el-button>
                     </template>
                     <template slot="require" slot-scope="{ row }">
                         <el-tag :type="row.is_required ? 'success' : ''">{{row.is_required ? '是' : '否'}}</el-tag>
@@ -50,7 +96,15 @@
                 </com-table>
             </div>
             <template v-else>
-                <el-button class="mt20 mb15" size="small" type="primary" @click="relationOperate('add')">
+                <el-button
+                    v-permission="{
+                        id: $route.name,
+                        type: 'ModelManage_create'
+                    }"
+                    class="mt20 mb15"
+                    size="small"
+                    type="primary"
+                    @click="relationOperate('add')">
                     添加关联
                 </el-button>
                 <com-table
@@ -74,8 +128,26 @@
                         {{ showConnectName(row) }}
                     </template>
                     <template slot="operation" slot-scope="{ row }">
-                        <el-button type="text" size="mini" @click="relationOperate('edit',row)">编辑</el-button>
-                        <el-button type="text" size="mini" @click="deleteAsso(row)">删除</el-button>
+                        <el-button
+                            v-permission="{
+                                id: $route.name,
+                                type: 'ModelManage_edit'
+                            }"
+                            type="text"
+                            size="mini"
+                            @click="relationOperate('edit',row)">
+                            编辑
+                        </el-button>
+                        <el-button
+                            v-permission="{
+                                id: $route.name,
+                                type: 'ModelManage_delete'
+                            }"
+                            type="text"
+                            size="mini"
+                            @click="deleteAsso(row)">
+                            删除
+                        </el-button>
                     </template>
                 </com-table>
             </template>
@@ -95,329 +167,8 @@
     </div>
 </template>
 
-<script lang="ts">
-    import { Component, Vue, Watch } from 'vue-property-decorator'
-    import AddModel from '../components/modelOperation.vue'
-    import MenuTab from '@/components/menuTab/index.vue'
-    import ComTable from '@/components/comTable/index.vue'
-    import AttributesOperation from '../components/attributesOperation.vue'
-    import ConnectOperation from '../components/connectOperation.vue'
-    import { TableData } from '@/common/types'
-    import { PROPERTY_COLUMNS, OPERATE_TYPE_LIST, RELATION_COLUMNS, TARGET_BIND_LIST } from '@/common/constants/assetManage/modelManage'
+<script lang="ts" src="./index.ts"></script>
 
-    @Component({
-        components: {
-            AddModel,
-            MenuTab,
-            ComTable,
-            AttributesOperation,
-            ConnectOperation
-        }
-    })
-    export default class ModelManage extends Vue {
-        panels: Array<any> = [
-            { name: 'property', label: '字段属性' },
-            { name: 'relation', label: '关联关系' }
-        ]
-        active: string = 'property'
-        columns: Array<TableData> = PROPERTY_COLUMNS
-        relateColumns: Array<TableData> = RELATION_COLUMNS
-        propertyData: Array<any> = []
-        relationData: Array<any> = []
-        loading: boolean = false
-        relateLoading: boolean = false
-        pageOccupiedHeight: number = 400
-        modelGroupList: Array<any> = []
-        modelList: Array<any> = []
-        modelInfoList: Array<any> = []
-        connectTypeList: Array<any> = []
-
-        get powerParams() {
-            return {}
-        }
-
-        get modelInfo() {
-            const modelInfo:any = this.$route.query.modelInfo || '{}'
-            return JSON.parse(modelInfo)
-        }
-
-        @Watch('active')
-        onAlreadyAddListChanged(val) {
-            if (val) {
-                val === 'property' ? this.getModelAttrList() : this.getModelAssoList()
-            }
-        }
-
-        mounted() {
-            this.getAllModelList()
-        }
-
-        showMapping(row) {
-            return TARGET_BIND_LIST.find(item => item.id === row.mapping)?.name || '--'
-        }
-        showModelName(id) {
-            return this.modelInfoList.find(item => item.model_id === id)?.model_name || '--'
-        }
-        showConnectType(id, key) {
-            return this.connectTypeList.find(item => item.id === id)?.[key] || '--'
-        }
-
-        showConnectName(row) {
-            const sourceName = this.showModelName(row.src_model_id)
-            const targetName = this.showModelName(row.dst_model_id)
-            const relation = this.showConnectType(row.asst_id, 'name')
-            return `${sourceName} ${relation} ${targetName}`
-        }
-        getAllModelList() {
-            this.loading = true
-            Promise.all([this.getClassification(), this.getModelInfoList(), this.getModelAttrList('init'), this.getConnectTypeList()]).finally(() => {
-                this.modelList = this.modelGroupList.map(item => {
-                    item.list = this.modelInfoList.filter(tex => tex.classification_id === item.classification_id)
-                    return item
-                })
-                this.loading = false
-            })
-        }
-
-        showAttrType(type) {
-            if (type === 'organization') return '组织'
-            return OPERATE_TYPE_LIST.find(item => item.id === type)?.name || '--'
-        }
-
-        relationOperate(type, row = {}) {
-            if (!this.$BtnPermission(this.powerParams)) {
-                return false
-            }
-            const connectOperation: any = this.$refs.connectOperation
-            connectOperation.show(type, row)
-        }
-        async getModelAssoList() {
-            try {
-                const params = {
-                    id: this.modelInfo.model_id
-                }
-                this.relateLoading = true
-                const { result, message, data } = await this.$api.ModelManage.getModelAssoList(params)
-                if (!result) {
-                    return this.$error(message)
-                }
-                this.relationData = data
-            } catch (e) {
-                console.error(e)
-            } finally {
-                this.relateLoading = false
-            }
-        }
-
-        async getConnectTypeList() {
-            const { result, data } = await this.$api.ModelManage.getAssotypeList()
-            if (!result) {
-                return false
-            }
-            this.connectTypeList = data.map(item => {
-                return {
-                    id: item.asst_id,
-                    label: item.asst_name,
-                    name: `${item.asst_name}(${item.asst_id})`
-                }
-            })
-        }
-
-        async getModelAttrList(type?) {
-            try {
-                const params = {
-                    id: this.modelInfo.model_id
-                }
-                this.loading = true
-                const { result, message, data } = await this.$api.ModelManage.getModelAttrList(params)
-                if (!result) {
-                    return this.$error(message)
-                }
-                this.propertyData = data
-            } catch (e) {
-                console.error(e)
-            } finally {
-                this.loading = type === 'init'
-            }
-        }
-
-        toTabMenu(item) {
-            this.active = item.name
-        }
-
-        editAttr(row) {
-            if (!this.$BtnPermission(this.powerParams)) {
-                return false
-            }
-            const attributesOperation: any = this.$refs.attributesOperation
-            attributesOperation.show({
-                id: this.modelInfo.model_id,
-                type: row ? '编辑' : '添加',
-                rowData: row,
-                groupList: []
-            })
-        }
-
-        editModel() {
-            if (!this.$BtnPermission(this.powerParams)) {
-                return false
-            }
-            const editModel: any = this.$refs.editModel
-            editModel.show('edit', this.modelInfo)
-        }
-
-        async getClassification() {
-            const { result, message, data } = await this.$api.ModelManage.getClassification()
-            if (!result) {
-                return this.$error(message)
-            }
-            this.modelGroupList = data
-        }
-
-        async getModelInfoList() {
-            const { result, message, data } = await this.$api.ModelManage.getModel()
-            if (!result) {
-                return this.$error(message)
-            }
-            this.modelInfoList = data
-        }
-
-        refreshModel(params) {
-            this.$router.replace({
-                name: 'ModelDetail',
-                query: {
-                    modelInfo: JSON.stringify(params)
-                }
-            })
-        }
-
-        // 删除模型
-        handleDelete(row) {
-            if (!this.$BtnPermission({
-                id: this.$route.name,
-                type: 'SysRole_permissions'
-            })) {
-                return false
-            }
-            const params:any = {
-                id: this.modelInfo.model_id
-            }
-            this.$confirm('确认要删除此模型？', '提示', {
-                center: true
-            }).then(() => {
-                this.deleteModel(params)
-            })
-        }
-
-        async deleteModel(params) {
-            const res = await this.$api.ModelManage.deleteModel(params)
-            if (!res.result) {
-                return this.$error(res.message)
-            }
-            this.$success('删除成功')
-            this.$router.go(-1)
-        }
-        // 删除模型
-        deleteAsso(row) {
-            if (!this.$BtnPermission({
-                id: this.$route.name,
-                type: 'SysRole_permissions'
-            })) {
-                return false
-            }
-            const params:any = {
-                id: row.model_asst_id
-            }
-            this.$confirm('确认要删除此关联？', '提示', {
-                center: true
-            }).then(() => {
-                this.deleteModelAsso(params)
-            })
-        }
-
-        async deleteModelAsso(params) {
-            const res = await this.$api.ModelManage.deleteAssociation(params)
-            if (!res.result) {
-                return this.$error(res.message)
-            }
-            this.$success('删除成功')
-            this.getModelAssoList()
-        }
-
-        // 删除属性
-        deleteAtrr(row) {
-            if (!this.$BtnPermission({
-                id: this.$route.name,
-                type: 'SysRole_permissions'
-            })) {
-                return false
-            }
-            const params:any = {
-                id: this.modelInfo.model_id,
-                attr_id: row.attr_id
-            }
-            this.$confirm('确认要删除此属性？', '提示', {
-                center: true
-            }).then(() => {
-                this.deleteModelAttr(params)
-            })
-        }
-
-        async deleteModelAttr(params) {
-            const res = await this.$api.ModelManage.deleteModelAttr(params)
-            if (!res.result) {
-                return this.$error(res.message)
-            }
-            this.$success('删除成功')
-            this.getModelAttrList()
-        }
-
-        handlerChange() {
-            this.getModelAttrList()
-        }
-    }
-</script>
-
-<style lang="scss" scoped>
-.model-detail {
-    .detail-header {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        border-radius: 4px;
-        background-image: url(~@/assets/img/model_bg.png);
-        box-shadow: 0 2px 12px 0 rgba(0, 0, 0, .1);
-        padding: 20px 60px 20px 20px;
-        .header-info {
-            display: flex;
-            align-items: center;
-            .icon {
-                cursor: pointer;
-                height: 50px;
-                margin: 0 auto;
-                width: 50px;
-                display: flex;
-                align-items: center;
-                justify-content: center;
-                img {
-                    width: 24px;
-                    height: 24px;
-                }
-            }
-            .object {
-                margin: 0 20px;
-                .object-id {
-                    font-size: 12px;
-                    color: #C9CDD4;
-                }
-            }
-            .asset {
-                .asset-num {
-                    color: #409EFF;
-                    border-bottom: 1px solid #409EFF;
-                }
-            }
-        }
-    }
-}
+<style lang="scss">
+@import "./index.scss"
 </style>
