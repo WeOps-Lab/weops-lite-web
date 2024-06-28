c6de2b6ac9ff756b7765d5ff083ce115f6129674
CharlesWhj <1253297146@qq.com>
2024-06-28 10:19:16 +0800

    bugfix: 凭据模型不需要查看拓扑

--- src/views/asset/assetData/components/assoInfo/index/index.vue
+++ src/views/asset/assetData/components/assoInfo/index/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="asso-info">
-        <el-radio-group v-model="active" size="small" class="mb20">
+        <el-radio-group v-model="active" size="small" class="mb20" v-if="instInfo.model_type !== 'credential'">
             <el-radio-button label="列表"></el-radio-button>
             <el-radio-button label="拓扑"></el-radio-button>
         </el-radio-group>
